901ede766d52cb6584d42f34c5a521d678c1bebe
DominicHudson102983679 <61397178+DominicHudson102983679@users.noreply.github.com>
2020-08-13 13:54:28 +1000

    ...

--- AngularWeek3App/src/app/app.component.ts
+++ AngularWeek3App/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Contact } from './contact/contact';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularWeeeeeeeeeeeeeeeeeeeek3App';
+  title = 'Week3';
+  
+  contacts: Contact[] = [
+    new Contact("Bob", "Mx", "1 Queen Street, Melbs"),
+    new Contact("Frank", "Mr", "2 Queen Street, Melbs"),
+    new Contact("Jess", "Ms", "3 Queen Street, Melbs"),
+    new Contact("George", "Mx", "4 Queen Street, Melbs"),
+  ];
+
+
+showContacts: boolean = true;
+
+onToggleContacts() {
+  // if(this.showContacts == true){
+  //   this.showContacts = false;
+  // }else {
+  //   this.showContacts = true;
+  // }
+  this.showContacts = !this.showContacts;
+}
+  
 }
